096e86ecd88f44be5aed256f911e215540365c1f
Lucas Kent <rubickent@gmail.com>
2019-03-15 08:20:27 +1100

    Display variable addresses in hex

--- rust/src/process_scripts.rs
+++ rust/src/process_scripts.rs
@@ -1,4 +1,14 @@
 use brawllib_rs::script_ast::{EventAst, ForLoop, Iterations, IfStatement, Expression, UnaryExpression, BinaryExpression, ChangeAction};
+use brawllib_rs::script_ast::variable_ast::{
+    VariableAst,
+    InternalConstantInt,
+    LongtermAccessInt,
+    LongtermAccessFloat,
+    LongtermAccessBool,
+    RandomAccessInt,
+    RandomAccessFloat,
+    RandomAccessBool,
+};
 
 use crate::brawl_data::{BrawlMod, BrawlFighter};
 
@@ -81,7 +91,7 @@ pub fn process_events(events: &[EventAst], common: bool, brawl_mod: &BrawlMod, f
                     if let Some(script) = fighter.fighter.scripts_section.iter().find(|x| x.callers.contains(&offset.origin)) {
                         result.push_str(&format!("<li>Goto(<a href='help'>External: {}</a>)</li>", script.name));
                     } else {
-                        result.push_str(&format!("<li>Subroutine(Offset {{ offset: 0x{:x}, origin: 0x{:x} }})</li>", offset.offset, offset.origin));
+                        result.push_str(&format!("<li>Goto(Offset {{ offset: 0x{:x}, origin: 0x{:x} }})</li>", offset.offset, offset.origin));
                         error!("Failed to lookup script for goto destination");
                     }
                 }
@@ -93,8 +103,8 @@ pub fn process_events(events: &[EventAst], common: bool, brawl_mod: &BrawlMod, f
                     if let Some(script) = fighter.fighter.scripts_section.iter().find(|x| x.callers.contains(&offset.origin)) {
                         result.push_str(&format!("<li>Subroutine(<a href='/{}/{}/scripts_common/{}.html'>External: {}</a>)</li>", brawl_mod.name, fighter.fighter.name, script.name, script.name));
                     } else {
-                        result.push_str(&format!("<li>Goto(Offset {{ offset: 0x{:x}, origin: 0x{:x} }})</li>", offset.offset, offset.origin));
-                        error!("Failed to lookup script for goto destination");
+                        result.push_str(&format!("<li>Subroutine(Offset {{ offset: 0x{:x}, origin: 0x{:x} }})</li>", offset.offset, offset.origin));
+                        error!("Failed to lookup script for subroutine destination");
                     }
                 }
             }
@@ -110,6 +120,33 @@ pub fn process_events(events: &[EventAst], common: bool, brawl_mod: &BrawlMod, f
                     }
                 }
             }
+            EventAst::IntVariableSet { value, variable } => result.push_str(&format!("<li>IntVariableSet {{ variable: {}, value: {} }}</li>", process_expression(&Expression::Variable(variable.clone())), value)),
+            EventAst::IntVariableAdd { value, variable } => result.push_str(&format!("<li>IntVariableAdd {{ variable: {}, value: {} }}</li>", process_expression(&Expression::Variable(variable.clone())), value)),
+            EventAst::IntVariableSubtract { value, variable } => result.push_str(&format!("<li>IntVariableSubtract {{ variable: {}, value: {} }}</li>", process_expression(&Expression::Variable(variable.clone())), value)),
+            EventAst::IntVariableIncrement { variable } => result.push_str(&format!("<li>IntVariableIncrement {{ variable: {} }}</li>", process_expression(&Expression::Variable(variable.clone())))),
+            EventAst::IntVariableDecrement { variable } => result.push_str(&format!("<li>IntVariableDecrement {{ variable: {} }}</li>", process_expression(&Expression::Variable(variable.clone())))),
+            EventAst::FloatVariableSet { value, variable } => result.push_str(&format!("<li>FloatVariableSet {{ variable: {}, value: {} }}</li>", process_expression(&Expression::Variable(variable.clone())), value)),
+            EventAst::FloatVariableAdd { value, variable } => result.push_str(&format!("<li>FloatVariableAdd {{ variable: {}, value: {} }}</li>", process_expression(&Expression::Variable(variable.clone())), value)),
+            EventAst::FloatVariableSubtract { value, variable } => result.push_str(&format!("<li>FloatVariableSubtract {{ variable: {}, value: {} }}</li>", process_expression(&Expression::Variable(variable.clone())), value)),
+            EventAst::FloatVariableMultiply { value, variable } => result.push_str(&format!("<li>FloatVariableMultiply {{ variable: {}, value: {} }}</li>", process_expression(&Expression::Variable(variable.clone())), value)),
+            EventAst::FloatVariableDivide { value, variable } => result.push_str(&format!("<li>FloatVariableDivide {{ variable: {}, value: {} }}</li>", process_expression(&Expression::Variable(variable.clone())), value)),
+            EventAst::BoolVariableSetTrue { variable } => result.push_str(&format!("<li>BoolVariableSetTrue {{ variable: {} }}</li>", process_expression(&Expression::Variable(variable.clone())))),
+            EventAst::BoolVariableSetFalse { variable } => result.push_str(&format!("<li>BoolVariableSetFalse {{ variable: {} }}</li>", process_expression(&Expression::Variable(variable.clone())))),
+            EventAst::ItemThrow { unk1, unk2, unk3, unk4, unk5 } => result.push_str(&format!("<li>ItemThrow {{ unk1: {}, unk2: {}, unk3: {} unk4: {}, unk5: {} }}</li>",
+                process_expression(&Expression::Variable(unk1.clone())),
+                process_expression(&Expression::Variable(unk2.clone())),
+                process_expression(&Expression::Variable(unk3.clone())),
+                unk4.as_ref().map(|x| process_expression(&Expression::Variable(x.clone()))).unwrap_or("None".into()),
+                unk5.as_ref().map(|x| process_expression(&Expression::Variable(x.clone()))).unwrap_or("None".into()),
+            )),
+            EventAst::ItemThrow2 { unk1, unk2, unk3 } => result.push_str(&format!("<li>ItemThrow2 {{ unk1: {}, unk2: {}, unk3: {}}}</li>", unk1, unk2, process_expression(&Expression::Variable(unk3.clone())))),
+            EventAst::ApplyThrow (throw) => result.push_str(&format!("<li>ApplyThrow {{ unk0: {}, bone: {}, unk1: {} unk2: {}, unk3: {} }}</li>",
+                throw.unk0,
+                throw.bone,
+                process_expression(&Expression::Variable(throw.unk1.clone())),
+                process_expression(&Expression::Variable(throw.unk2.clone())),
+                process_expression(&Expression::Variable(throw.unk3.clone())),
+            )),
             EventAst::Unknown (event) => result.push_str(&format!("<li>UnknownEvent {{ namespace: 0x{:x}, code: 0x{:x}, unk1: 0x{:x}, arguments: {:?} }}</li>", event.namespace, event.code, event.unk1, event.arguments)),
             _ => result.push_str(&format!("<li>{:?}</li>", event)),
         }
@@ -126,7 +163,20 @@ fn process_expression(expr: &Expression) -> String {
         Expression::Binary (BinaryExpression { left, operator, right })
             => format!("{} {:?} {}", process_expression(left), operator, process_expression(right)),
         Expression::Not (expr) => format!("not({})", process_expression(expr)),
-        Expression::Variable (variable) => format!("{:?}", variable),
+        Expression::Variable (variable) =>
+            // TODO: This handler needs to be also called by stuff like IntVariableSet
+            match variable {
+                VariableAst::InternalConstantInt (InternalConstantInt::Address (address)) => format!("InternalConstantInt (0x{:x})", address),
+                VariableAst::LongtermAccessInt   (LongtermAccessInt::Address   (address)) => format!("LongtermAccessInt (0x{:x})", address),
+                VariableAst::LongtermAccessFloat (LongtermAccessFloat::Address (address)) => format!("LongtermAccessFloat (0x{:x})", address),
+                VariableAst::LongtermAccessBool  (LongtermAccessBool::Address  (address)) => format!("LongtermAccessBool (0x{:x})", address),
+                VariableAst::RandomAccessInt     (RandomAccessInt::Address     (address)) => format!("RandomAccessInt (0x{:x})", address),
+                VariableAst::RandomAccessFloat   (RandomAccessFloat::Address   (address)) => format!("RandomAccessFloat (0x{:x})", address),
+                VariableAst::RandomAccessBool    (RandomAccessBool::Address    (address)) => format!("RandomAccessBool (0x{:x})", address),
+                VariableAst::Unknown { memory_type, data_type, address } =>
+                    format!("Unknown {{ memory_type: {:?}, data_type: {:?}, address: 0x{:x} }}", memory_type, data_type, address),
+                _ => format!("{:?}", variable),
+            }
         Expression::Value (value) => format!("value({})", value),
         Expression::Scalar (scalar) => format!("scalar({})", scalar),
     }
