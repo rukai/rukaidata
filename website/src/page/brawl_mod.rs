fda27724547bcde27e18b260d8d35e45d57aa549
Rukai <rubickent@gmail.com>
2019-05-21 16:59:08 +1000

    Revert previous commit
    The previous commit was made under the assumption that s3fs/goofys would
    be a good way to deploy changes.
    However it was rediculously slow, so now we are just syncing pregenerated pages.
    
    Generating this "Coming soon" pages no longer works because it would overwrite pages
    that the generator has no way of knowing about.

--- website/src/page/brawl_mod.rs
+++ website/src/page/brawl_mod.rs
@@ -1,5 +1,4 @@
 use std::fs::File;
-use std::path::Path;
 use std::fs;
 
 use handlebars::Handlebars;
@@ -10,28 +9,7 @@ use crate::assets::AssetPaths;
 
 pub fn generate(handlebars: &Handlebars, brawl_mods: &BrawlMods, assets: &AssetPaths) {
     for brawl_mod in &brawl_mods.mods {
-        fs::create_dir_all(format!("../root/{}", brawl_mod.name)).unwrap();
-        let index_path = format!("../root/{}/index.html", brawl_mod.name);
-
-        if brawl_mod.dummy {
-            // If the page exists, do not write ANYTHING.
-            // Dummy mods are used to retain the pages for an already generated mod.
-            // However we still want to create a placeholder page when the mod is new so the user doesnt get linked to a 404.
-            //
-            // One case we cant handle is updating a placeholder page with a new placeholder page.
-            // But this is unlikely to be needed so not worth worrying about.
-            if !Path::new(&index_path).exists() {
-                let page = ModPage {
-                    mod_links:     brawl_mods.gen_mod_links(brawl_mod.name.clone()),
-                    title:         format!("{} Coming Soon!", brawl_mod.name),
-                    fighter_links: vec!(),
-                    assets,
-                };
-                let file = File::create(index_path).unwrap();
-                handlebars.render_to_write("mod", &page, file).unwrap();
-            }
-        }
-        else {
+        if !brawl_mod.dummy {
             let mut fighter_links = vec!();
             for fighter in &brawl_mod.fighters {
                 fighter_links.push(NavLink {
@@ -48,7 +26,9 @@ pub fn generate(handlebars: &Handlebars, brawl_mods: &BrawlMods, assets: &AssetP
                 assets,
             };
 
-            let file = File::create(index_path).unwrap();
+            fs::create_dir_all(format!("../root/{}", brawl_mod.name)).unwrap();
+            let path = format!("../root/{}/index.html", brawl_mod.name);
+            let file = File::create(path).unwrap();
             handlebars.render_to_write("mod", &page, file).unwrap();
         }
     }
