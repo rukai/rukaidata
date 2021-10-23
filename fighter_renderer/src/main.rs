6e1229d7bf6bf3a311d21d86b46459dfb8689139
Lucas Kent <rubickent@gmail.com>
2021-10-23 16:00:55 +1100

    Implement frame setting

--- fighter_renderer/src/main.rs
+++ fighter_renderer/src/main.rs
@@ -2,13 +2,12 @@ use brawllib_rs::high_level_fighter::HighLevelSubaction;
 use brawllib_rs::renderer::app::state::{AppEvent, State};
 use brawllib_rs::renderer::app::App;
 
+use log::Level;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{Document, HtmlElement};
 use web_sys::{Request, RequestInit, RequestMode, Response};
-use log::Level;
-
 
 fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
@@ -40,7 +39,9 @@ async fn get_subaction(subaction_name: &str) -> HighLevelSubaction {
     let request = Request::new_with_str_and_init(&url, &opts).unwrap();
 
     let window = web_sys::window().unwrap();
-    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await.unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request))
+        .await
+        .unwrap();
 
     // `resp_value` is a `Response` object.
     assert!(resp_value.is_instance_of::<Response>());
@@ -54,18 +55,34 @@ async fn get_subaction(subaction_name: &str) -> HighLevelSubaction {
 
 pub async fn run_renderer(document: Document, subaction: HighLevelSubaction) {
     let visualiser_span = document.get_element_by_id("fighter-render").unwrap();
+    let frames_len = subaction.frames.len();
     let app = App::new_insert_into_element(visualiser_span, subaction).await;
 
     let event_tx = app.get_event_tx();
 
-    let frame = document.get_element_by_id("frame").unwrap();
-    let frame_move = frame.clone();
-    frame_move.set_inner_html("Frame: 0");
+    for i in 0..frames_len {
+        let event_tx = event_tx.clone();
+        let set_frame = Closure::wrap(Box::new(move || {
+            event_tx.send(AppEvent::SetState(State::Pause)).unwrap();
+            event_tx.send(AppEvent::SetFrame(i)).unwrap();
+        }) as Box<dyn FnMut()>);
 
-    let button = document.get_element_by_id("run").unwrap();
+        let button = document
+            .get_element_by_id(&format!("set_frame_{}", i + 1))
+            .unwrap();
+        button
+            .dyn_ref::<HtmlElement>()
+            .unwrap()
+            .set_onclick(Some(set_frame.as_ref().unchecked_ref()));
+
+        // Need to forget closure otherwise the destructor destroys it ;-;
+        set_frame.forget();
+    }
+
+    let button = document.get_element_by_id("run-toggle").unwrap();
     let button_move = button.clone();
     button_move.set_inner_html("Run");
-    let do_thing = Closure::wrap(Box::new(move || {
+    let run_toggle = Closure::wrap(Box::new(move || {
         if button_move.inner_html() == "Stop" {
             event_tx.send(AppEvent::SetState(State::Pause)).unwrap();
             button_move.set_inner_html("Run");
@@ -74,10 +91,14 @@ pub async fn run_renderer(document: Document, subaction: HighLevelSubaction) {
             button_move.set_inner_html("Stop");
         }
     }) as Box<dyn FnMut()>);
+
+    // TODO: receive start/stop messages and set button_move accordingly
+    // TODO: receive current frame messages and update set_frame to add class .current-frame-background
+
     button
         .dyn_ref::<HtmlElement>()
         .unwrap()
-        .set_onclick(Some(do_thing.as_ref().unchecked_ref()));
+        .set_onclick(Some(run_toggle.as_ref().unchecked_ref()));
 
     app.get_event_tx()
         .send(AppEvent::SetState(State::Pause))
