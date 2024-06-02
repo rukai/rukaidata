3f1975a151c4e53aea1011e4f4fcd1071b6177f5
Lucas Kent <rubickent@gmail.com>
2024-06-02 17:08:54 +1000

    Use upstream tower-http (#42)

--- website/src/serve.rs
+++ website/src/serve.rs
@@ -1,4 +1,4 @@
-use axum::Router;
+use axum::{middleware::map_response, response::Response, routing, Router};
 use tower_http::services::ServeDir;
 
 pub fn serve() {
@@ -8,7 +8,11 @@ pub fn serve() {
 
 async fn run() {
     // build our application with a route
-    let app = Router::new().nest_service("/", ServeDir::new("../root").precompressed_gzip());
+    let app = Router::new().nest_service(
+        "/",
+        routing::get_service(ServeDir::new("../root"))
+            .layer(map_response(insert_compression_header)),
+    );
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
@@ -16,4 +20,13 @@ async fn run() {
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
+}
+
+// All files are compressed with gzip, so we need to overwrite content-encoding to let the client know.
+// We cant use `ServeDir::precompressed_gzip` since name on disk needs to match name served for hosting on AWS S3 to work.
+async fn insert_compression_header<B>(mut response: Response<B>) -> Response<B> {
+    response
+        .headers_mut()
+        .insert("content-encoding", "gzip".parse().unwrap());
+    response
 }
